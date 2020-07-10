8e8e74bb8e9a4f60200a63bc252b0ed4d01c2512
Videsh Loya <59529535+VideshLoya@users.noreply.github.com>
2020-07-10 11:42:49 +0530

    Adding Backend files

---
+++ Backend/controllers/image.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: 'YOUR_API_KEY_HERE'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    // This part has been updated with the recent Clarifai changes. Used to be:
+    // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
